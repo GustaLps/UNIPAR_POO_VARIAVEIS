eb1169733ee4c40efd371d21db8f864c57187c58
GustaLps <161260765+GustaLps@users.noreply.github.com>
2024-02-25 21:22:49 -0300

    AT2.java

---
+++ AT2.java
@@ -0,0 +1,14 @@
+class AT2 {
+    public static void main(String[] args) {
+        String Nome;
+        double Preco; 
+        String Marca; 
+        int Quantidade; 
+        String Descricao; 
+        String NomeDoProduto;
+        int Numero; 
+        double Preco; 
+        int Estoque; 
+        int Total;
+    }
+}
