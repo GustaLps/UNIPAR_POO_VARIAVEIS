aee97fdc266959f9caf967554685cd0f88b69f2f
GustaLps <161260765+GustaLps@users.noreply.github.com>
2024-02-25 21:32:58 -0300

    AT5.java

---
+++ AT5.java
@@ -0,0 +1,43 @@
+ public class AT5 {
+    public static void main(String[] args) {
+        
+        String Nome = "Iphone XR  ";
+        String Marca = "Apple";
+        double Preco = 2.000,00;
+        int Qtd = 100;  
+        
+        System.out.println("Dados do Smartphone:");
+        System.out.println("Nome: " + Nome);
+        System.out.println("Marca: " + Marca);
+        System.out.println("Preco: " + Preco);
+        System.out.println("Estoque: " + Qtd);
+       
+       System.out.println(" ");
+        
+        String nomeLaptop = "NP550XDA-KF4BR";
+        String marcaLaptop = "Sansung";
+        double precoLaptop = 2.800,00;
+        int qtdLaptop = 50;
+        
+        System.out.println("Dados do Laptop:");
+        System.out.println("Nome: " + nomeLaptop);
+        System.out.println("Marca: " + marcaLaptop);
+        System.out.println("Preco: " + precoLaptop);
+        System.out.println("Estoque: " + qtdLaptop);
+        
+        System.out.println(" ");
+        
+        String nomeFoneDeOuvido = "AirPods";
+        String marcaFoneDeOuvido = "Apple";
+        double precoFoneDeOuvido = 1100.00;
+        int qtdFoneDeOuvido = 40;
+        
+        System.out.println("Dados do Fone De Ouvido:");
+        System.out.println("Nome: " + nomeFoneDeOuvido);
+        System.out.println("Marca: " + marcaFoneDeOuvido);
+        System.out.println("Preco: " + precoFoneDeOuvido);
+        System.out.println("Estoque: " + qtdFoneDeOuvido);
+        
+      
+    }
+}
