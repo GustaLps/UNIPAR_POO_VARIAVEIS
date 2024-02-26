0f23b0a3e39d149f7e07884edd236c6ba3babcc5
GustaLps <161260765+GustaLps@users.noreply.github.com>
2024-02-25 21:20:13 -0300

    AT1.java

---
+++ AT1.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nome;
+        float valor = 2.4f;
+        int idade = 20;
+        Integer ano = 60;
+        double peso = 70.0;
+        boolean confirma = true;
+        char sexo = 'H';
+        long numG = 323432423434;
+        byte numP = 2334
+        short numC = 123
+    }
+}
