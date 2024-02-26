35da779052561969ded74b54b1414db19435fac0
GustaLps <161260765+GustaLps@users.noreply.github.com>
2024-02-25 21:27:05 -0300

    AT4.java

---
+++ AT4.java
@@ -0,0 +1,10 @@
+public class Controle {
+    public static void main(String[] args) {
+        // Declaração das variáveis aqui
+        String Nome;
+        String Categoria
+        double Preco;
+        int Qtd
+
+    }
+}
